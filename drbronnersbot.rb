7781dd4c9f25dd9f8e5b8c08f81ce8b2b2f08a5a
Gregg Horton <greggahorton@gmail.com>
2014-02-15 19:31:41 -0800

    adding markov madness

--- drbronnersbot.rb
+++ drbronnersbot.rb
@@ -1,15 +1,24 @@
 require 'twitter'
+require 'marky_markov'
 load 'client.rb'
-def pick_random_line
-  chosen_line = nil
-  File.foreach("label.txt").each_with_index do |line, number|
-    chosen_line = line if rand < 1.0/(number+1)
-  end
-  return chosen_line.scan(/.{1,140}/)
+
+# def pick_random_line
+#   chosen_line = nil
+#   File.foreach("label.txt").each_with_index do |line, number|
+#     chosen_line = line if rand < 1.0/(number+1)
+#   end
+#   return chosen_line.scan(/.{1,140}/)
+# end
+
+def drbronners
+  tweet = ""
+	markov = MarkyMarkov::Dictionary.new('dictionary')
+  markov.parse_file "label.txt"
+  tweet = markov.generate_n_words 15
+  markov.save_dictionary!
+  client = client_maker
+  client.update(tweet)
 end
 
 
-client = client_maker
-
-tweet = pick_random_line.join
-client.update(tweet)
+drbronners
